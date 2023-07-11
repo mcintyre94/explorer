1f5128679ff182b4d1c75ca5d6062b8523286ff2
Callum McIntyre <callum.mcintyre@solana.com>
2023-07-11 08:44:15 +0000

    Move localStorageIsAvailable util to its own file
    utils/index.ts has a dependency on legacy web3js

---
+++ app/utils/local-storage.ts
@@ -0,0 +1,10 @@
+export function localStorageIsAvailable() {
+  const test = 'test';
+  try {
+    localStorage.setItem(test, test);
+    localStorage.removeItem(test);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
